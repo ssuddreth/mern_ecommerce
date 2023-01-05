f080270cb652309db0593db77f039f76ab9fd8b8
Steven Suddreth <stevensuddreth@gmail.com>
2023-01-05 13:37:37 -0500

    update build command in API package-json

--- api/client/src/components/Products.jsx
+++ api/client/src/components/Products.jsx
@@ -21,8 +21,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://mern-ecommerce-app-ssuddreth.herokuapp.com/api/products?category=${cat}`
+            : "https://mern-ecommerce-app-ssuddreth.herokuapp.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {
